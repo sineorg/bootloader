f37dde00ddc4df62ce2ccb737b6196ef4ed3bf0f
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2025-10-27 18:39:40 -0400

    Remove module loader and replace with Sine initialization

--- program/config.js
+++ program/config.js
@@ -7,33 +7,7 @@ if (!Services.appinfo.inSafeMode) {
    
     if (cmanifest.exists()) {
       Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
-     
-      const MODULE_LOADER = ChromeUtils.compileScript("chrome://userchromejs/content/module_loader.mjs");
-      
-      Services.obs.addObserver({
-        observe(window) {
-          window.addEventListener("DOMContentLoaded", async (event) => {
-            const document = event.originalTarget;
-            const window = document.defaultView;
-
-            const path = window.location.pathname;
-            if (path !== "/content/browser.xhtml" && 
-                path !== "/content/messenger.xhtml" && 
-                path !== "settings" && 
-                path !== "preferences") return;
-           
-            document.allowUnsafeHTML = false;
-           
-            Object.defineProperty(window, "UC_API", {
-              get: () => ChromeUtils.importESModule("chrome://userchromejs/content/uc_api.sys.mjs"),
-              configurable: true,
-              enumerable: true
-            });
-           
-            MODULE_LOADER.then(m => m.executeInGlobal(window));
-          }, { capture: true, once: false, passive: true });
-        }
-      }, "domwindowopened", false);
+      ChromeUtils.importESModule("chrome://userscripts/content/sine.sys.mjs");
     }
   } catch (err) {}
 }
