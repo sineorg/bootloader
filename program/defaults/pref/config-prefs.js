f1a9af2584fc8f158389871f7cb6cf9c5f360b3c
CosmoCreeper <179134799+CosmoCreeper@users.noreply.github.com>
2026-03-03 09:31:46 -0500

    chore: Move filename config to first line
    Moves the config filename preference to the top of the script, making it easier to remove that line in the installer for Librewolf.

--- program/defaults/pref/config-prefs.js
+++ program/defaults/pref/config-prefs.js
@@ -1,4 +1,4 @@
-pref("general.config.obscure_value", 0);
 pref("general.config.filename", "config.js");
+pref("general.config.obscure_value", 0);
 // Sandbox needs to be disabled in release and Beta versions
 pref("general.config.sandbox_enabled", false);
